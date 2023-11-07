f07c139e626835a36f4e6b0b7cca87c0fc808790
VicentCode <vicentcode@gmail.com>
2023-11-07 09:52:57 -0600

    Added filter album on AlbumVisor

--- app/src/main/java/com/vicentcode/unidadll/Practicas/Fotos/ui/AlbumVisor.kt
+++ app/src/main/java/com/vicentcode/unidadll/Practicas/Fotos/ui/AlbumVisor.kt
@@ -1,6 +1,7 @@
 package com.vicentcode.unidadll.Practicas.Fotos.ui
 
 import android.Manifest
+import android.R
 import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.database.sqlite.SQLiteDatabase
@@ -9,6 +10,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -32,18 +35,52 @@ class AlbumVisor : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //deleteAllData()
-        if (checkPermission()) {
-            v.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            v.recyclerView.adapter = AlbumAdapter(getAllInfo("SELECT * FROM albums"))
-            v.recyclerView.setHasFixedSize(true)
-        } else {
-            // Si no se tienen los permisos, solicita permisos al usuario
-            v.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            v.recyclerView.adapter = AlbumAdapter(getAllInfo("SELECT * FROM albums"))
-            v.recyclerView.setHasFixedSize(true)
-            requestPermission()
+
+        //fill the spiner with all the albums names from the database
+        val spinner = v.filtersp
+        val adminSQLite = AlbumSQL(requireContext(), "fotitos", 2)
+        val bdconnector = adminSQLite.writableDatabase
+        val cursor = bdconnector.rawQuery("SELECT DISTINCT albumName FROM albums", null)
+        val dataList = ArrayList<String>()
+        dataList.add("Todos")
+        if (cursor.moveToFirst()) {
+            do {
+                dataList.add(cursor.getString(0))
+            } while (cursor.moveToNext())
         }
+        bdconnector.close()
+        val adapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, dataList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //if the user select an album from the spinner, show the photos from that album
+                if (position != 0) {
+                    v.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+                    v.recyclerView.adapter = AlbumAdapter(getAllInfo("SELECT * FROM albums WHERE albumName = '${dataList[position]}'"))
+                    v.recyclerView.setHasFixedSize(true)
+                    requestPermission()
+                } else {
+                    //if the user select the first option from the spinner, show all the photos from all the albums
+                    v.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+                    v.recyclerView.adapter = AlbumAdapter(getAllInfo("SELECT * FROM albums"))
+                    v.recyclerView.setHasFixedSize(true)
+                    requestPermission()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                v.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+                v.recyclerView.adapter = AlbumAdapter(getAllInfo("SELECT * FROM albums"))
+                v.recyclerView.setHasFixedSize(true)
+                requestPermission()
+            }
+
+        }
+
+
+
+
 
 
 
